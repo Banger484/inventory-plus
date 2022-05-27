4154fa6587996c1e7249e9d1b44b3fb71599219e
Joe Christianson <jcfargond@gmail.com>
2022-05-27 10:03:14 -0500

    changed user related mutations and created add items

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,6 +7,7 @@ type Enterprise {
   name: String!
   location: String!
   registrant: User!
+  users: [User]
 }
 
 type User {
@@ -14,9 +15,9 @@ type User {
     name: String!
     email: String!
     password: String!
-    credentials: String!
-    enterprise: String!
-    role: String!
+    credentials: String
+    enterprise: ID
+    role: String
   }
 
   type Product {
@@ -27,6 +28,11 @@ type User {
     msrp: Float!
     category: String!
     notes: String!
+  }
+
+  type Item {
+    _id: ID!
+    product: ID!
   }
 
   type Auth {
@@ -40,14 +46,18 @@ type User {
     allProducts: [Product]
     singleProduct(id:ID!): Product
     singleCategoryProducts(category: String!): [Product]
+    getEnterprises: [Enterprise]
+    getEnterpriseByUser(email:String!): Enterprise
+    getItems: [Item]
   }
 
   type Mutation {
-    addUser(name: String!, email: String!, password: String!,enterprise: String!,role: String!): Auth
+    addUser(name: String!, email: String!, password: String!,enterprise: String,role: String,credentials: String): Auth
     login(email: String!, password: String!): Auth
     addProduct(sku: String!, name: String!, description: String!, msrp: Int!, category: String! notes: String!): Product
     updateProduct(_id: ID!, sku: String, name: String, description: String, msrp: Int, category: String notes: String): Product
     addEnterprise(name:String!,location:String!,userId:ID!): Enterprise
+    addItems(quantity:Int!,productId:ID!,orderNumber:Int!,cost:Int!,purchaseDate:String!,supplier:String!,enterpriseId:ID!): [Item]
   }
 
 
