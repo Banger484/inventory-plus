2dd8fb6f736679ad9a18e73abc187474a61e38a1
abelzer09 <aaronrbelzer@gmail.com>
2022-05-31 15:28:25 -0500

    worked on emailjs functionality

--- client/src/components/Users/AddUser.js
+++ client/src/components/Users/AddUser.js
@@ -1,12 +1,25 @@
-
+import emailjs from '@emailjs/browser';
 
 export default function AddUser () {
+    const enterpriseId ="adfadfads5656153ds"
+    const link = `https://inventoryplus.herokuapp.com/invite/${enterpriseId}`
+    function sendEmail(e) {
+        e.preventDefault();
+        emailjs.sendForm('gmail', 'template_op0417e', e.target, 'LXAuyeYFRIN_Vzkeh')
+        console.log(e.target)
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    }
     return (
         <div>
             <h1>Invite User</h1>
-            <form>
-                <input name="email" type="email"></input>
-                <button type='submit'>Send Invite</button>
+            <form onSubmit= {sendEmail}>
+                <input name="user_email" type="email"></input>
+                <input name="enterpriseId" type="text" value={link}></input>
+                <button type='submit' value="Send">Send Invite</button>
             </form>
         </div>
     )
