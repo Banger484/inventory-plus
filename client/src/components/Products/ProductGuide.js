836d7a93c8c73ec3362b88a87e98918fc88f2780
Bret Banger <Banger484@gmail.com>
2022-06-02 12:45:28 -0500

    working on tables

--- client/src/components/Products/ProductGuide.js
+++ client/src/components/Products/ProductGuide.js
@@ -4,12 +4,14 @@ import ProductList from "./ProductList";
 export default function ProductGuide (props) {
     console.log(props)
     return (
-        <div className='product-guide-container'>
-            <div className='table-top'>
+        <>
+        <div className='table-top'>
             <h1>Product Guide</h1>
             </div>
-
+        <div className='product-guide-container'>
             <ProductList />
         </div>
+        </>
+        
     )
 }
