07d27c9e26375c9a039e0b524dbcd4d5152bcfc2
abelzer09 <aaronrbelzer@gmail.com>
2022-06-03 10:23:39 -0500

    added comments to mutation.js

--- server/schemas/mutation.js
+++ server/schemas/mutation.js
@@ -1,7 +1,9 @@
 const { Enterprise, Item, Product, User } = require('../models');
 const { signToken } = require('../utils/auth');
 
+// fuctions for manipulating database through mutations
 const mutation = {
+    // Function to update a single Items receivedDate and binLocation
     updateItemReceived: async (parent, {item, receivedDate, binLocation}) => {
         return await Item.findOneAndUpdate(
             {_id: item},
@@ -9,6 +11,7 @@ const mutation = {
             {new:true}
         );
     },
+    // Function to update a single Items saleDate, buyer, saleNumber, salePrice
     updateItemSale: async ( parent, {item, saleDate, buyer,saleNumber,salesPrice }) => {
         return await Item.findOneAndUpdate(
             {_id: item},
@@ -16,6 +19,7 @@ const mutation = {
             {new:true}
         );
     },
+    // Function to update a single Items receivedDate and binLocation
     updateItemFulf: async (parent, {item, fulfillmentDate}) => {
         return await Item.findOneAndUpdate(
             {_id: item},
@@ -23,9 +27,9 @@ const mutation = {
             {new:true}
         );
     },
+    // Function to create a new Enterprise and register new User for the Enterprise
     register : async (parent,{name,email,password,location,enterpriseName})=>{
         try{
-
             const enterprise = await Enterprise.create({name:enterpriseName,location})
             console.log(enterprise)
             const user = await User.create({name,email,password,enterprise:enterprise._id})
@@ -37,16 +41,18 @@ const mutation = {
             console.log(err)
         }
     },
+    // Function to register a new User to an Enterprise
     registerOnlyUser: async (parent,{name,email,password,enterprise})=>{
         const user = await User.create({name,email,password,enterprise});
         let result = await User.findById(user._id).populate("enterprise")
         return result;
     },
+    // Function to update a single Items receivedDate and binLocation
     removeUser: async (parent,{userId})=>{
         const removed = await User.findOneAndDelete({_id:userId})
         return removed
-    }
-    ,
+    },
+    // Function to login in a user to application
     login: async (parent, { email, password }) => {
         console.log("in it")
         console.log({email,password})
@@ -66,8 +72,9 @@ const mutation = {
         const finder = await Enterprise.findOne({_id:user.enterprise})
         user.enterprise = finder
         return { token, user };
-      },
-      addToOrderGuide: async (parent,{enterpriseId,productId})=>{
+    },
+    
+    addToOrderGuide: async (parent,{enterpriseId,productId})=>{
         const ent = await Enterprise.findById(enterpriseId);
         ent.orderGuide.push(productId)
         await ent.save()
