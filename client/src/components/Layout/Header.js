a0a8285847afa0429b00b2c6558052994765c9b3
Joe Christianson <jcfargond@gmail.com>
2022-06-27 14:03:57 -0500

    add missing code for avatar

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -5,15 +5,16 @@ import React from 'react'
 import Icon from './Icon'
 
 // Export function to display header of application with welcome enterprise and user. Displays application logo and name as well.
-export default function Header(props) {
+export default function Header({user,enterprise}) {
+  
   return (
     <header className='header-grid'>
         <div className='header-avatar'>
-        <img src='/images/icons/avatar.png' alt='your avatar'/>
+        {user.avatar?(<div className='avatar-cont'><img id="avatar-image" className='avatar-image' src={`/images/${user.avatar}`}/></div>):(<img src='/images/icons/avatar.png' alt='your avatar'/>)}
         </div>
         <div className='header-welcome'>
-        <h1>{props.enterprise}</h1>
-        <p>Welcome, {props.user}.</p>
+        <h1>{enterprise}</h1>
+        <p>Welcome, {user.name}.</p>
         </div>
         <div className='header-logo'>
         <Icon /> 
