9a20bf40502a71954f5d9fa3cb1375e865d278e1
Bret Banger <Banger484@gmail.com>
2022-06-27 14:05:10 -0500

    gonna pull

--- client/src/components/Users/Settings.js
+++ client/src/components/Users/Settings.js
@@ -1,29 +1,30 @@
-import { themes } from "../../themes"
-import { useMutation } from "@apollo/client"
-import { SET_THEME } from "../../utils/mutations"
+import { themes } from "../../themes";
+import { useMutation } from "@apollo/client";
+import { SET_THEME } from "../../utils/mutations";
 
+export const Settings = ({ handleThemeChange, user }) => {
+  const [setTheme, { data: setThemeData, error: setThemeError }] =
+    useMutation(SET_THEME);
 
-export const Settings = ({handleThemeChange,user})=>{
+  const handleThemeChangeAndSetThemeForUser = async (e) => {
+    handleThemeChange(e);
+    const variables = { userId: user.data._id, theme: e.target.value };
+    console.log(variables);
+    const resp = await setTheme({ variables });
+    console.log(resp);
+  };
 
-
-const [setTheme,{data:setThemeData,error:setThemeError}] = useMutation(SET_THEME)
-
-
-const handleThemeChangeAndSetThemeForUser = async (e)=>{
-    handleThemeChange(e)
-    const variables = {userId:user.data._id,theme:e.target.value};
-    console.log(variables)
-    const resp = await setTheme({variables})
-    console.log(resp)
-}
-
-    return (
-        <div className="settings-menu">
-            <select onChange={handleThemeChangeAndSetThemeForUser}>
-                {Object.keys(themes).map(k=>{
-                    return(<option value={k}>{k}</option>)
-                })}
-                </select>
-        </div>
-    )
-}
+  return (
+    <div className="settings-container">
+      <div className="settings-menu">
+        <h1>User Preferences</h1>
+        <img src={`/images/${user.avatar}`}></img>
+        <select onChange={handleThemeChangeAndSetThemeForUser}>
+          {Object.keys(themes).map((k, index) => {
+            return <option key={index} value={k}>{k}</option>;
+          })}
+        </select>
+      </div>
+    </div>
+  );
+};
